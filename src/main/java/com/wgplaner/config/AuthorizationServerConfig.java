48198b7636edff96542d480534c229f99febfbee
paul <diljosepaul@gmail.com>
2023-02-25 13:33:26 +0100

    upgrade to 1.0.1, defaultSecConfig in same file

--- src/main/java/com/wgplaner/config/AuthorizationServerConfig.java
+++ src/main/java/com/wgplaner/config/AuthorizationServerConfig.java
@@ -9,11 +9,14 @@ import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -22,65 +25,89 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
         return http.cors().configurationSource(request -> {
                     CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:19006", "https://auth.expo.io/",
-                            "https://auth.expo.io/wg-planer/login", "https://auth.expo.io/--/wg-planer/login", "exp://172.20.10.3:19000/--/wg-planer/login")); //TODO set this later
+                    corsConfiguration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:19006",
+                            "https://auth.expo.io/",
+                            "https://auth.expo.io/wg-planer/login",
+                            "https://auth.expo.io/--/wg-planer/login",
+                            "exp://172.20.10.3:19000/--/wg-planer/login"));
                     corsConfiguration.setAllowCredentials(true);
-                    corsConfiguration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name()));
+                    corsConfiguration.setAllowedMethods(
+                            Arrays.asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name()));
                     //corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
                     corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
                     corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
                     corsConfiguration.setMaxAge(1800L);
-                    //corsConfiguration.applyPermitDefaultValues();
                     return corsConfiguration;
                 }).and()
-                .formLogin(Customizer.withDefaults()).build();
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(
+                                new LoginUrlAuthenticationEntryPoint("/login"))
+                )
+                // Accept access tokens for User Info and/or Client Registration
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt).build();
     }
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("localhost:19006"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
-//        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
-//        configuration.setMaxAge(3600L);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
-//    @Bean
-//    public WebMvcConfigurer corsMappingConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("*")
-//                        .allowedMethods("*")
-//                        .allowedHeaders("*")
-//                        .exposedHeaders("*");
-//            }
-//        };
-//    }
+    @Bean
+    @Order(2)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:19006",
+                            "https://auth.expo.io/",
+                            "https://auth.expo.io/wg-planer/login",
+                            "https://auth.expo.io/--/wg-planer/login",
+                            "exp://172.20.10.3:19000/--/wg-planer/login"));
+                    corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name()));
+                    corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+                    corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+                    corsConfiguration.setMaxAge(1800L);
+                    //corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+                    return corsConfiguration;
+                }).and()
+                .authorizeHttpRequests().requestMatchers("/actuator/**").permitAll().and()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
@@ -91,15 +118,12 @@ public class AuthorizationServerConfig {
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
-                //.redirectUri("http://127.0.0.1:8080/login/oauth2/code/wg-planer")
                 .redirectUri("http://127.0.0.1:19006/wg-planer/login")
                 .redirectUri("https://auth.expo.io/paulo48/wg-planer-mobile")
                 .redirectUri("https://auth.expo.io/wg-planer/login")
                 .redirectUri("https://auth.expo.io/--/wg-planer/login")
                 .redirectUri("exp://172.20.10.3:19000/--/wg-planer/login")
                 .scope(OidcScopes.OPENID)
-//                .scope("articles.read")
                 .build();
 
         return new InMemoryRegisteredClientRepository(registeredClient);
@@ -139,22 +163,8 @@ public class AuthorizationServerConfig {
         return keyPair;
     }
 
-    //@Bean
-    //public ProviderSettings providerSettings() {
-    //    return ProviderSettings.builder()
-    //            .issuer("https://wg-planer-auth-server.onrender.com")
-    //            //.issuer("http://localhost:8080")
-    //            .build();
-    //}
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder()
-                //.issuer("https://wg-planer-auth-server.onrender.com")
-                .issuer("http://localhost:8080")
-                .tokenIntrospectionEndpoint("/oauth2/introspect")
-                .tokenRevocationEndpoint("/oauth2/revoke")
-                .oidcUserInfoEndpoint("/connect/userinfo")
-                .oidcClientRegistrationEndpoint("/connect/register")
-                .build();
+        return AuthorizationServerSettings.builder().build();
     }
 }
